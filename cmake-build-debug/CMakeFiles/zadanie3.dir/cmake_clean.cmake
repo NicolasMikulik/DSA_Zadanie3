6565239fffc5ca2a9c7d94756793dcb25f3a5420
Nicolas Mikulik <nicolas.mikulik01@gmail.com>
2019-11-19 12:26:11 +0100

    pt5

---
+++ cmake-build-debug/CMakeFiles/zadanie3.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/zadanie3.dir/main.c.o"
+  "zadanie3.pdb"
+  "zadanie3"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/zadanie3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
